3b748ce4000f65e2c615fb349e4a95586e80fd50
koya <koyagabriel@gmail.com>
2022-01-26 17:41:59 +0100

    adds logic for instantiating flask app

---
+++ manage.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from app import create_app
+
+dotenv_path: str  = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+app: Flask = create_app(os.environ.get("FLASK_ENV") or "development")
+
