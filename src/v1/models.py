e664f1d76652848e5cce8dd2a76b0ac15bebaef0
koya <koyagabriel@gmail.com>
2022-01-29 15:49:48 +0100

    adds method to fetch best value games

--- src/v1/models.py
+++ src/v1/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from pydoc import classname
 from app import db
+from typing import List
 
 
 class BaseMixin(object):
@@ -31,4 +33,23 @@ class Game(BaseMixin, db.Model):
         if many:
             return query_set.all()
         return query_set.first()
+    
+    @classmethod
+    def fetch_best_value_games(cls, pen_drive_space: int):
+        query_set = cls.query.filter(cls.space <= pen_drive_space).order_by(cls.space)
+        list_of_games: List[cls]  = []
+        total_space: int = 0
+        remaining_space: int = pen_drive_space
+        for game in query_set:
+            game_space: int = game.space
+            if remaining_space >= game_space:
+                list_of_games.append(game)
+                total_space += game_space
+                remaining_space -= game_space
+            else:
+                break
+        
+        return (list_of_games, total_space, remaining_space)
+
+        
 
