54ae3172c79cc1890302bdfe4c5645f9e8a2b057
koya <koyagabriel@gmail.com>
2022-01-29 14:20:10 +0100

    adds ValidationError to catch_exception decorator

--- src/v1/decorators.py
+++ src/v1/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from marshmallow import ValidationError
 
 from src.v1.errors import custom_error, validation_error
 from src.v1.exceptions import ValidationException, CustomException
@@ -18,6 +19,8 @@ def catch_exceptions(default_error_message):
                 if e.should_propagate:
                     return e.broadcast()
                 return validation_error(default_error_message)
+            except ValidationError as e:
+                return validation_error(e.messages)
             except BaseException as e:
                 return custom_error(default_error_message)
         return decorated_function
